7fbfa2a85262f116e1db0b7a23e79d6887efbd6b
Josh <josh@Joshui-Macmini.local>
2026-02-09 23:27:07 +0900

    feat: MVP Ready (Dark Mode, 3-Column Grid)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    preview: {
+      allowedHosts: true, // Allow all hosts for tunnel testing
+    },
+    server: {
+      allowedHosts: true,
+    }
+  }
+});
